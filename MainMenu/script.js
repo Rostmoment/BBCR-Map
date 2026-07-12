a7f7e8c5d9465f07785abfc6f26ea0f115403092
Rost Moment <110627353+Rostmoment@users.noreply.github.com>
2026-07-12 23:15:16 +0300

    Removed theme button because I have no idea how to make it work on every page without making code bad

--- MainMenu/script.js
+++ MainMenu/script.js
@@ -5,26 +5,7 @@ function addHoverStyleText(element, text) {
     });
 }
 
-function initializeThemeChanges() {
-    const themeToggleBtn = document.getElementById('theme-toggle');
-    const savedTheme = localStorage.getItem('theme');
 
-    if (savedTheme) {
-        document.documentElement.setAttribute('data-theme', savedTheme);
-    }
-    themeToggleBtn.addEventListener('click', () => {
-        let currentTheme = document.documentElement.getAttribute('data-theme');
-
-        if (currentTheme === 'dark') {
-            document.documentElement.setAttribute('data-theme', 'light');
-            localStorage.setItem('theme', 'light');
-        } else {
-            document.documentElement.setAttribute('data-theme', 'dark');
-            localStorage.setItem('theme', 'dark');
-        }
-    });
-    addHoverStyleText(themeToggleBtn, "Change theme to dark or light whenever you want")
-}
 function initializeCredits() {
     const credit = document.getElementById("credits-button");
     addHoverStyleText(credit, "Credits of this site");
@@ -44,6 +25,5 @@ function initializeStyles() {
     initializeStyle("demo-style-door", "Demo");
 }
 
-initializeThemeChanges();
 initializeCredits();
 initializeStyles();
