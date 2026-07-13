174cebafbb68bdc45460ef72a4f33f18ba9a0619
Rost Moment <110627353+Rostmoment@users.noreply.github.com>
2026-07-13 15:53:54 +0300

    TAB icons for styles

---
+++ Styles/main.js
@@ -0,0 +1,8 @@
+const style = new URLSearchParams(window.location.search).get("style");
+document.title = `${style} Style`;
+
+const newFavicon = document.createElement("link");
+newFavicon.rel = "icon";
+newFavicon.type = "image/png";
+newFavicon.href = `../Icons/${style}.png`;
+document.head.appendChild(newFavicon);
--- Styles/script.js
+++
@@ -1,2 +0,0 @@
-const style = new URLSearchParams(window.location.search).get("style");
-document.title = `${style} Style`;
